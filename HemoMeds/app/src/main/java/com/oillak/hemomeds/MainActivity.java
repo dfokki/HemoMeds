10355dbe02d41f7e6c3b5db36187f439f0a6b2da
dfokki <sonata_jjk@hotmail.com>
2017-04-12 15:18:28 +0300

    added layouts calendar,patient_main

--- HemoMeds/app/src/main/java/com/oillak/hemomeds/MainActivity.java
+++ HemoMeds/app/src/main/java/com/oillak/hemomeds/MainActivity.java
@@ -1,7 +1,15 @@
 package com.oillak.hemomeds;
 
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
+import android.icu.util.TimeZone;
+import android.icu.util.ULocale;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.DatePicker;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatePicker datePicker = (DatePicker)findViewById(R.id.datePicker);
+
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("/E/d/MMMM/yyyy/HH:mm",Locale.getDefault());
+        String strDate = sdf.format(calendar.getTime());
+
+        String[] values=strDate.split("/",0);
+
+        for (int i = 0; i < values.length; i++
+                ){
+            Log.v("CHECK_DATE", values[i]);
+        }
+
     }
+
+
 }
